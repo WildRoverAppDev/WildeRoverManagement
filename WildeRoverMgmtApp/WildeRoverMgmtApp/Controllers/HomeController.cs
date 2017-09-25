66c49ba62cacc33deb49bf896680606a53c21ce0
RizRam <riz.ramdhani@gmail.com>
2017-09-24 19:40:35 -0700

    +Minor Bug fix for InventorySummary/Submit.cshtml Page +Improved Home/Index Page

--- WildeRoverMgmtApp/WildeRoverMgmtApp/Controllers/HomeController.cs
+++ WildeRoverMgmtApp/WildeRoverMgmtApp/Controllers/HomeController.cs
@@ -4,14 +4,63 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using WildeRoverMgmtApp.Models;
 
 namespace WildeRoverMgmtApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly WildeRoverMgmtAppContext _context;
+        private const int MAX_ENTRIES = 10; 
+
+        public HomeController(WildeRoverMgmtAppContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+
+            var invLogs = (from log in _context.InventoryLog
+                           orderby log.Date descending
+                           select log);
+
+            var ordLogs = from log in _context.OrderLog
+                          orderby log.Date descending
+                          select log;
+
+            HomeIndexViewModel model = new HomeIndexViewModel();
+
+            int counter = 0;
+            foreach(var log in invLogs)
+            {
+                if (counter < MAX_ENTRIES)
+                {
+                    model.InventoryList.Add(log);
+                    counter++;
+                }
+                else
+                {
+                    break;
+                }                
+            }
+
+            counter = 0;
+            foreach(var log in ordLogs)
+            {
+                if (counter < MAX_ENTRIES)
+                {
+                    model.OrderList.Add(log);
+                    counter++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return View(model);
         }
 
         public IActionResult About()
